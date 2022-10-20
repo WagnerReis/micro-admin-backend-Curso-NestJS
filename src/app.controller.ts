180d48612489ff0f29dda1493e5eb895e6b3dae0
WagnerReis <wagnerreis1997@gmail.com>
2022-10-20 16:30:01 -0300

    Microservices: NestJS, RabbitMQ and Cloud Services

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Logger } from '@nestjs/common';
 import { AppService } from './app.service';
-
+import { Payload, EventPattern } from '@nestjs/microservices';
+import { Categoria } from './interfaces/categorias/categoria.interface';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  logger = new Logger(AppController.name);
+
+  @EventPattern('criar-categoria')
+  async criarCategoria(@Payload() categoria: Categoria) {
+    this.logger.log(`categoria: ${JSON.stringify(categoria)}`);
+
+    this.appService.criarCategoria(categoria);
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CategoriaSchema } from './interfaces/categorias/categoria.schema';
+import { JogadorSchema } from './interfaces/jogadores/jogador.schema';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://admin:XzpOKbZcTqZohu8r@cluster0.slukmih.mongodb.net/sradminbackend?retryWrites=true&w=majority',
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      },
+    ),
+    MongooseModule.forFeature([
+      { name: 'Categoria', schema: CategoriaSchema },
+      { name: 'Jogador', schema: JogadorSchema },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
